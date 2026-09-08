0c5d173401ac2dc28c59469a63856b1cd0887bfd
FAnizah29 <karatekat211@gmail.com>
2026-09-08 23:47:10 +0300

    FINALLY COMPILED CPP PROGRAM (AI DID IT )

---
+++ good_night.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    std::cout << "It is almsost 12 am, i ahve school tmorwo and i ahve spend like 3 hours just reconriugring my files and folders and FIGURITIN OTU HOW TO COMPIKLE AND RUN C++ WHICH IS HARD FO RNO REASON ))l::::. \n";
+
+    std::string answer;
+    std::cout << "Would u like to sleep?";
+    std::getline(std::cin, answer);
+
+    std::cout << "well it dont mateter you are lsleepign OK!!";
+}
